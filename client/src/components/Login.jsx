a7e18a5bf27c0b393a8fd7c6b067cd2029434b5c
namanrox <naman.deep991@gmail.com>
2024-05-06 19:59:21 +0530

    login and signup completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog">
+          <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
             {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
@@ -19,8 +28,14 @@ function Login() {
                 type="email"
                 placeholder="Enter your email address here..."
                 className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
               />
               <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
             </div>
 
             <div className="mt-4 space-y-2">
@@ -30,8 +45,14 @@ function Login() {
                 type="password"
                 placeholder="Enter your password"
                 className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
               />
               <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
             </div>
 
             <div className="flex justify-around mt-6">
@@ -40,9 +61,12 @@ function Login() {
               </button>
               <p>
                 Not registered?{" "}
-                <a href="/signup" className="underline text-blue-500 cursor-pointer">
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
                   SignUp
-                </a>
+                </Link>
               </p>
             </div>
           </form>
