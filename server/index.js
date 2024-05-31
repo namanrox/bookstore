b18b6aa7e7b22181e0a732d1d676e13cb3be1729
namanrox <naman.deep991@gmail.com>
2024-05-31 18:08:39 +0530

    simplified

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   const dirPath = path.resolve();
   app.use(express.static("../client/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(dirPath, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(dirPath, "../client/dist", "index.html"));
   });
 }
 
